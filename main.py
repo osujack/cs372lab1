7aba7b10b218406f82c423f6c2274e29491be778
Jackson Lee <leejacks@oregonstate.edu>
2024-01-17 15:48:25 -0800

    1/17 1520 Draft 2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# Author: Jackson Lee
-# GitHub username: osujack
-# Date: 01/17/2023
-# Lab: 1
-# Course: CS 372 Winter 2024
-# Description:   Create a simple python program that uses a socket to interact with a server.
+#       Author: Jackson Lee
+#         Date: 01/17/2023
+#       Course: CS 372 Winter 2024
+#          Lab: 1
+#  Description: Create a simple python program that uses a socket to interact with a server.
 #
 
+# import socket.py module to use the python socket API.
 import socket
 
 def interact_with_server():
@@ -21,7 +21,8 @@ def interact_with_server():
         client_socket.connect((server_address, server_port))
 
         # Send a simple HTTP GET request
-        request = "GET / HTTP/1.1\r\nHost: {}\r\n\r\n".format(server_address)
+        request = "GET /wireshark-labs/HTTP-wireshark-file3.html HTTP/1.1\r\nHost:{}\r\n\r\n".format(server_address)
+
         client_socket.sendall(request.encode())
 
         # Receive and print the server's response
