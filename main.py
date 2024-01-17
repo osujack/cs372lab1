074cc6fe11a72fa600a6340f00ff4f0cd81b75f0
Jackson Lee <leejacks@oregonstate.edu>
2024-01-17 15:11:20 -0800

    1/17 1511 Test Commit.

---
+++ main.py
@@ -0,0 +1,19 @@
+# Author: Jackson Lee
+# GitHub username: osujack
+# Date: 01/17/2023
+# Lab: 1
+# Course: CS 372 Winter 2024
+# Description:   Create a simple python program that uses a socket to interact with a server.
+#
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
