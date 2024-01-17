7cef4551d2c7798a73bfe26241ade777a87102c9
Jackson Lee <leejacks@oregonstate.edu>
2024-01-17 15:21:16 -0800

    1/17 1520 Draft 1

--- main.py
+++ main.py
@@ -6,14 +6,38 @@
 # Description:   Create a simple python program that uses a socket to interact with a server.
 #
 
+import socket
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def interact_with_server():
+    # Server information
+    server_address = "gaia.cs.umass.edu"
+    server_port = 80  # HTTP port
 
+    # Create a socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    try:
+        # Connect to the server
+        client_socket.connect((server_address, server_port))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # Send a simple HTTP GET request
+        request = "GET / HTTP/1.1\r\nHost: {}\r\n\r\n".format(server_address)
+        client_socket.sendall(request.encode())
+
+        # Receive and print the server's response
+        response = b""
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            response += data
+
+        print("Server Response:")
+        print(response.decode())
+
+    finally:
+        # Close the socket
+        client_socket.close()
+
+if __name__ == "__main__":
+    interact_with_server()
